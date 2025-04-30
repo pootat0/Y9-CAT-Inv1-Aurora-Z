31daa506de7a0c42a4dec6d29f74837ea8b4c844
Aurora <boya.zhao@student.education.wa.edu.au>
2025-04-30 18:11:57 -0400

    added input helper

--- #Population Growth Modeling.py
+++ #Population Growth Modeling.py
@@ -1,3 +1,12 @@
 #Population Growth Modeling
 import time
 import math
+
+#input helper
+def input_number(prompt, dtype=float):
+    while True:
+        value = input(prompt)
+        try:
+            return dtype(value)
+        except ValueError:
+            print("Please enter a valid number.")
